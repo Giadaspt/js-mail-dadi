50f861f635593bd59736f16b858af4455f745d07
Giadaspt <saputo.giada@gmail.com>
2021-10-26 15:03:10 +0200

    primo commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Mail
+/*
+.1 creare una lista di email finte degli eventuali utenti, quindi creare un array con tutte le email.
+.2 chiedere all'utente tramite prompt di inserire la sua email.
+.3 controllare che l'email sia valida oppure no
+.4 output dicendo all'uitente che l'email è valida (perchè nella lista), oppure no
+*/
+
+const email = [
+  'perdigon@sewce.com',
+  'perdigon@gmailni.com',
+  'perdigon@noisemails.com',
+  'perdigon@teeoli.com',
+  'perdigon@onlinecmail.com',
+  'perdigon@howti.site',
+  'perdigon@howti.site',
+  'perdigon@howti.site',
+  'perdigon@beaplumbereducationok.sale',
+  'perdigon@psasey.site',
+  'perdigon@ccvisal.xyz',
+  'perdigon@1ki.co',
+  'perdigon@psasey.site',
+  'perdigon@mexcool.com',
+]
